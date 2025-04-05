56e7e776276307d5e4e168dc944af2dbefc4d49e
박상일 <sangil6372@naver.com>
2025-04-06 00:20:06 +0900

    init: 초기 설정 추가 (FontAwesome, Calendar, Proxy 설정)

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 // 메인 CSS
-import './assets/main.css';
+import "./assets/main.css";
 // 부트스트랩
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; 
+import "@fortawesome/fontawesome-free/css/all.min.css"; 
 
-import { createApp } from 'vue';
+import { createApp } from "vue";
 
-import App from './App.vue';
-import router from './router';
-import { createPinia } from 'pinia';
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,20 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src'),
+      '@fullcalendar/core': path.resolve(__dirname, 'node_modules/@fullcalendar/core'),
+      '@fullcalendar/daygrid': path.resolve(__dirname, 'node_modules/@fullcalendar/daygrid'),
+      '@fullcalendar/interaction': path.resolve(__dirname, 'node_modules/@fullcalendar/interaction')
     },
   },
-})
+  server: {
+    proxy: {
+      // '/api'로 시작하는 요청은 http://localhost:3000으로 보냄
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // '/api' 접두사 제거
+      }
+    }
+  },
+});
