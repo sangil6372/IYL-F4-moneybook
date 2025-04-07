cee5426118e4d4fd62eac29941308cf4e0a60bae
박상일 <sangil6372@naver.com>
2025-04-08 01:04:25 +0900

    fix : user 내부에 setting 포함 및 프로필 이미지 상태도 저장

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -17,6 +17,12 @@ export const useAuthStore = defineStore("auth", {
           name,
           email,
           password,
+          profileImage: null, // 프로필 이미지 초기값은 null
+          // 기본 settings 값을 설정하여 추가
+          settings: {
+            language: "kor", // 기본 언어 설정
+            theme: "light", // 기본 테마 설정
+          },
         });
         if (response.status === 201) {
           alert("회원가입이 완료되었습니다.");
@@ -40,19 +46,7 @@ export const useAuthStore = defineStore("auth", {
           this.user = user;
           localStorage.setItem("user", JSON.stringify(user)); // 사용자 정보도 저장
 
-          // settings 추가  <--- 일단 나중에 진행
-          //   const settingsResponse = await axios.get(
-          //     `http://localhost:3000/settings?userId=${user.id}`
-          //   );
-          //   if (settingsResponse.data.length === 0) {
-          //     // 없는 경우 추가(첫 생성)
-          //     await axios.post("http://localhost:3000/settings", {
-          //       userId: user.id,
-          //       notifications: true,
-          //       language: "kor",
-          //       theme: "light",
-          //     });
-          //   }
+          // settings 테이블을 추가하려고 했는데 그냥 속성으로 추가
         } else {
           throw new Error("이메일 혹은 비밀번호가 일치하지 않습니다.");
         }
@@ -66,10 +60,11 @@ export const useAuthStore = defineStore("auth", {
       this.user = null;
       localStorage.removeItem("user");
     },
-    // Rotuer의 네비게이션 가드에서 체크함 
+    // Rotuer의 네비게이션 가드에서 체크함
     loadUserFromStorage() {
       const user = localStorage.getItem("user");
-      if (user) { // 여기선 일단 단순히 user가 localStorage에 있으면 로그인 중이라고 체크
+      if (user) {
+        // 여기선 일단 단순히 user가 localStorage에 있으면 로그인 중이라고 체크
         this.user = JSON.parse(user);
       }
     },
