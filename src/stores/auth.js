dee9b4cfabe59943dd223c20345f479772177262
박상일 <sangil6372@naver.com>
2025-04-07 22:32:49 +0900

    fix : token 방식 제거 및및 로그인 시 name도 같이 입력 받도록 수정

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -4,17 +4,15 @@ import axios from "axios";
 export const useAuthStore = defineStore("auth", {
   state: () => ({
     user: null,
-    token: localStorage.getItem("token") || "",
   }),
   getters: {
     isLoggedIn: (state) => !!state.user,
     userName: (state) => (state.user ? state.user.name : ""),
   },
   actions: {
-    async register(email, password) {  // 회원 가입 로직 
+    async register(name, email, password) {
+      // 회원 가입 로직
       try {
-        let name = "";  // 일단 임시로 이름은 공백
-        //  이곳에 로직 필요! <-- 유효성 검사 
         const response = await axios.post("http://localhost:3000/users", {
           name,
           email,
@@ -36,26 +34,25 @@ export const useAuthStore = defineStore("auth", {
           `http://localhost:3000/users?email=${email}&password=${password}`
         );
 
-        if (response.data.length > 0) { // 정상적으로 반환하면 
+        if (response.data.length > 0) {
+          // 정상적으로 반환하면
           const user = response.data[0];
           this.user = user;
-          this.token = btoa(`${user.email}:${user.password}`); // Base64 암호화
-          localStorage.setItem("token", this.token);
           localStorage.setItem("user", JSON.stringify(user)); // 사용자 정보도 저장
 
-        // settings 추가  <--- 일단 나중에 진행
-        //   const settingsResponse = await axios.get(
-        //     `http://localhost:3000/settings?userId=${user.id}`
-        //   );
-        //   if (settingsResponse.data.length === 0) {
-        //     // 없는 경우 추가(첫 생성)
-        //     await axios.post("http://localhost:3000/settings", {
-        //       userId: user.id,
-        //       notifications: true,
-        //       language: "kor",
-        //       theme: "light",
-        //     });
-        //   }
+          // settings 추가  <--- 일단 나중에 진행
+          //   const settingsResponse = await axios.get(
+          //     `http://localhost:3000/settings?userId=${user.id}`
+          //   );
+          //   if (settingsResponse.data.length === 0) {
+          //     // 없는 경우 추가(첫 생성)
+          //     await axios.post("http://localhost:3000/settings", {
+          //       userId: user.id,
+          //       notifications: true,
+          //       language: "kor",
+          //       theme: "light",
+          //     });
+          //   }
         } else {
           throw new Error("이메일 혹은 비밀번호가 일치하지 않습니다.");
         }
@@ -64,17 +61,16 @@ export const useAuthStore = defineStore("auth", {
         throw error;
       }
     },
-    logout() {   // 상태 초기화 
+    logout() {
+      // 상태 초기화
       this.user = null;
-      this.token = "";
-      localStorage.removeItem("token");
       localStorage.removeItem("user");
     },
+    // Rotuer의 네비게이션 가드에서 체크함 
     loadUserFromStorage() {
       const user = localStorage.getItem("user");
-      if (user) {
+      if (user) { // 여기선 일단 단순히 user가 localStorage에 있으면 로그인 중이라고 체크
         this.user = JSON.parse(user);
-        this.token = localStorage.getItem("token");
       }
     },
   },
