4f67718f07c6d8247bd461734646880cb8377683
guno-p <sushieatinggg@gmail.com>
2025-04-09 10:28:31 +0900

    fix: userId에 따라서 거래목록 사용하도록 수정

--- src/stores/transaction.js
+++ src/stores/transaction.js
@@ -7,10 +7,14 @@ import { ref, reactive, computed } from 'vue';
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
+// userID 쓰기 위해 피니아 임포트
+import { useAuthStore } from '@/stores/auth';
+
 export const useTransaction = defineStore('transaction', () => {
   // state
   const BASEURI = '/api/transaction';
   const states = reactive({ transaction: [] });
+  const authStore = useAuthStore();
 
   // getter
   // computed로 관리되는 거래 목록
@@ -21,8 +25,18 @@ export const useTransaction = defineStore('transaction', () => {
   const fetchTransaction = async () => {
     try {
       const response = await axios.get(BASEURI);
+
+      // userId 가져오기
+      const userId = authStore.user.id;
+
+      // userId 에 따라서 거래목록을 필터링
+      const userTransaction = response.data.filter(item => {
+        return item.userId === userId;
+      });
+
       if (response.status === 200) {
-        states.transaction = response.data;
+        states.transaction = userTransaction;
+        console.log(states.transaction);
         console.log('거래 목록 갱신 완료');
       } else {
         alert('거래 목록 조회 실패');
